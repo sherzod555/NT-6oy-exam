7cb53b2df335ba69e1ef0ab65e53f6a80b965d49
sherzod555 <wiut00007584@gmail.com>
2023-10-27 05:40:21 +0500

    page->video.jsx responsive qoldi

--- src/pages/video/video.jsx
+++ src/pages/video/video.jsx
@@ -16,6 +16,7 @@ const Video = () => {
         `https://youtube-v31.p.rapidapi.com/videos?part=contentDetails%2Csnippet%2Cstatistics&id=${videoId}`
       );
       setVideoData(data);
+      console.log(data);
     }
 
     try {
@@ -49,61 +50,64 @@ const Video = () => {
 
   return (
     <>
-      <div className="container mx-auto px-[75px] pb-12">
-        <div className="w-[70%] pt-[110px]">
+      <div className="container mx-auto px-4 sm:px-[75px] pb-12">
+        <div className="sm:w-full md:w-[70%] pt-[110px]">
           <div className="pb-5 border-b-2 border-b-[#C2C2C2]">
             <img
               className="w-full h-auto rounded-2xl"
               src={videoData?.items[0].snippet.thumbnails.maxres.url}
               alt={videoData?.items[0].snippet.title}
             />
-            <h2 className="text-3xl font-semibold pt-5">
+            <h2 className="md:text-3xl sm:text-2xl text-xl font-semibold pt-3 sm:pt-5">
               {videoData?.items[0].snippet.title}
             </h2>
-            <div className="pt-3 flex justify-between items-center">
-              <p className="text-[#C2C2C2] text-sm">
+            <div className="sm:pt-3 pt-1 grid  lg:flex justify-between items-center">
+              <p className="text-[#C2C2C2] text-xs sm:text-sm">
                 {formatViewCount(videoData?.items[0].statistics.viewCount)}{" "}
                 views
               </p>
-              <div className="flex items-center gap-x-[10px]">
-                <div className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
+              <div className="mt-5 lg:mt-0 md:text-base text-xs flex items-center gap-x-[10px]">
+                <button className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
                   <img src={Like_icon} alt="like" />
                   <p>
                     {formatViewCount(videoData?.items[0].statistics.likeCount)}{" "}
                     Likes
                   </p>
-                </div>
-                <div className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
+                </button>
+                <button className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
                   <img className="-scale-y-100" src={Like_icon} alt="dislike" />
                   <p>Dislikes</p>
-                </div>
-                <div className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
+                </button>
+                <button className="flex gap-x-2 items-center px-5 py-[8px] rounded-[20px] bg-[#EBEBEB] hover:bg-[#d5d5d5]">
                   <img src={Share_icon} alt="share" />
                   <p>Share</p>
-                </div>
-                <div>
+                </button>
+                <button>
                   <img className="ml-[30px]" src={Moredots_icon} alt="more" />
-                </div>
+                </button>
               </div>
             </div>
           </div>
 
-          <div>
-            <div className="pt-10">
+          <div className="sm:flex pt-6 sm:pt-10 sm:items-start">
+            <div className="sm:flex sm:gap-x-3">
               <img
                 className="w-20 h-20 rounded-full"
                 src={videoData?.items[0].snippet.thumbnails.default.url}
                 alt="channel img"
               />
-              <div>
+              <div className="flex flex-col justify-center">
                 <h2>{videoData?.items[0].snippet.channelTitle}</h2>
-                <p>
+                <p className="sm:pt-1 text-[#C2C2C2] text-[13px]">
                   Published on{" "}
                   {formatDate(videoData?.items[0].snippet.publishedAt)}
                 </p>
-                <p>{videoData?.items[0].snippet.description}</p>
+                <p className="sm:pt-[14px] text-black opacity-60 md:w-3/4 line-clamp-6">{videoData?.items[0].snippet.description}</p>
               </div>
             </div>
+            <button className="hover:bg-[gray] bg-[#FF0000] text-white rounded-[20px] sm:px-5 sm:py-2">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
